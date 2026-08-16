110edaffac9778eabe3d6efd0c1f75f7ab76fea0
Kawser Hamim <kawser.hamim.456@gmail.com>
2026-08-16 20:49:30 +0600

    password

--- src/services/token-service.js
+++ src/services/token-service.js
@@ -7,7 +7,7 @@ export function signAuthToken(user) {
   return jwt.sign(
     { id: user._id, role: user.role },
     process.env.JWT_ACCESS_SECRET,
-    { expiresIn: "1h" }
+    { expiresIn: "24h" }
   );
 }
 
