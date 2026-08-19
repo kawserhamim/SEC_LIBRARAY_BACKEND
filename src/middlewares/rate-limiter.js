254ea6ce3cb938cb04d2aee6cef0659a1fd5fbae
Kawser Hamim <kawser.hamim.456@gmail.com>
2026-08-19 16:56:13 +0600

    final socket

--- src/middlewares/rate-limiter.js
+++ src/middlewares/rate-limiter.js
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit";
 
 export const authRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  limit: 20,
+  windowMs: 1 * 60 * 1000, // 1 minute window
+  limit: 5, // max 5 requests per minute
   standardHeaders: true,
   legacyHeaders: false,
   message: {
